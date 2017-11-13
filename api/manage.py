fcb3b03b74c231b070f9aebd3ec8e8a0b90fdb61
fabianfalon <fabian.falon@gmail.com>
2017-11-13 17:30:12 -0300

    Se agregan test simples para probar ci

--- api/manage.py
+++ api/manage.py
@@ -6,12 +6,22 @@
 """
 import logging
 import sys
-
+import unittest
+import coverage
 from flask.ext.script import Manager
 from api.db.syncdb import CreateUserCommand
 from api import create_app
 from flask_migrate import Migrate, MigrateCommand
 from api.core import BaseModel
+
+COV = coverage.coverage(
+    branch=True,
+    include='api/*',
+    omit=[
+        'api/tests/*',
+    ]
+)
+COV.start()
 
 root = logging.getLogger()
 root.setLevel(logging.INFO)
@@ -33,6 +43,32 @@ manager.add_command('db', MigrateCommand)
 
 root = logging.getLogger()
 root.setLevel(logging.INFO)
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('api/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('api/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 if __name__ == "__main__":
     manager.run()
